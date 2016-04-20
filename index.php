8f60be242129378c19aff7e52dd40e95590b12c5
jorisclaes <saxefoon@hotmail.com>
2016-04-20 12:46:37 +0200

    init

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Swagg Clan Official Site</title>
+</head>
+<body>
+    <?php
+        echo "<p> echo test </p>"
+    ?>
+</body>
+</html>
