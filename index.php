cd163b0800a5311affb8b4c45c0c8d490aa23144
SpookyHammer <gkaan2793@hotmail.com>
2016-04-22 09:58:04 +0200

    "Basis" layout gemaakt (dit mag nog veranderen)

--- index.php
+++ index.php
@@ -5,8 +5,26 @@
     <title>Swagg Clan Official Site</title>
 </head>
 <body>
-    <?php
-        echo "<p> echo test </p>"
-    ?>
+<!-- Main Logo -->
+<header>
+
+</header>
+<!-- Upper Menu-->
+<div id="topmenu">
+    <ul >
+        <li>Menu1</li>
+        <li>Menu2</li>
+        <li>Menu3</li>
+        <li>Menu4</li>
+    </ul>
+</div>
+<!-- Main Page -->
+<div>
+
+</div>
+<!-- Footer -->
+<footer>
+
+</footer>
 </body>
 </html>
